c7c9898366865290eca58b3c3e87f09a9c6977e2
velkyam <velkyam@hotmail.com>
2023-11-12 21:28:43 -0800

    poster

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import poster from './assets/images/poster.jpg'
 import video from './assets/images/Untitled.mp4'
 import video2 from './assets/images/Untitled.webm'
 import Typewriter from 'typewriter-effect';
@@ -28,10 +29,10 @@ function Hero() {
 klíč. Prostřednictvím našich špičkových SCADA systémů Vám můžeme usnadnit řízení
 provozu a zvýšit jeho efektivitu.
                         </div>
-                        <Link to='/melkorvisum'> <button type="button" className="btn btn-success btn-lg learnButton melkorButton ">Více o našem řešení</button></Link>
+                        <Link to='/melkorvisum' className='middleButton'> <button type="button" className="btn btn-success btn-lg learnButton melkorButton ">Více o našem řešení</button></Link>
                     </div>
                     <div className='videoContainer'>
-                        <video loop autoPlay muted playsInline className='bgVideo'>
+                        <video loop autoPlay muted playsInline poster={poster} className='bgVideo'>
                             <source src={video2} type="video/webm" />
                             <source src={video} type="video/mp4" />
                             <p>Your browser doesn't support HTML video. Here is a</p>
